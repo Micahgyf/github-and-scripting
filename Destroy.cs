0d51155c1830a13f02c4dd9420264d0c8e432897
Micah Goh <143924861+Micahgyf@users.noreply.github.com>
2025-01-12 12:26:34 +0800

    Create Destroy.cs

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroys gameObject at the start of the game
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
